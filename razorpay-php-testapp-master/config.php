c629dbb5cec6afd5a551d5966ac2f65c6e50ce78
root <email4arun@gmail.com>
2021-01-08 18:55:53 +0530

    Adding ignored config file

---
+++ razorpay-php-testapp-master/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$keyId = 'rzp_test_s9vY8m69TuXuxg';
+$keySecret = 'B0AxD1Dvpgx33QoxDok32wt3';
+$displayCurrency = 'INR';
+$baseUrl="http://localhost/writeneat";
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
